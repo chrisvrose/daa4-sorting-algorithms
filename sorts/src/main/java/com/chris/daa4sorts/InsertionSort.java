707901262b12e2cbe10042904d39df46ade8d720
Christopher V. Rose <christophervrose@gmail.com>
2020-04-21 12:51:17 +0530

    Finish Insertion Sorting

---
+++ sorts/src/main/java/com/chris/daa4sorts/InsertionSort.java
@@ -0,0 +1,40 @@
+package com.chris.daa4sorts;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class InsertionSort<T> extends GenericSort<T> {
+    private long count = 0;
+
+    public InsertionSort(ArrayList<T> elements, Comparator<T> comparator) {
+        this.elements = elements;
+        this.comparator = comparator;
+        count = 0;
+    }
+
+    public void sort() {
+        int size = elements.size();
+        for (int i = 1; i < size; ++i) {
+            T key = elements.get(i);
+            int j = i - 1;
+
+            /*
+             * Move elements of arr[0..i-1], that are greater than key, to one position
+             * ahead of their current position
+             */
+            while (j >= 0 &&  comparator.compare(elements.get(j), key)>0) {
+                elements.set(j+1,elements.get(j));
+                this.count++;
+                j--;
+            }
+            elements.set(j+1, key);
+        }
+
+        //done
+    }
+
+    public long getCount() {
+        return this.count;
+    }
+
+}
