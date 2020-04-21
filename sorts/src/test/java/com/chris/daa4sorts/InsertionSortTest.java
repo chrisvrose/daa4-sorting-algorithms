b5807b981ec49a716ac46d51597906a49fc5327c
Christopher V. Rose <christophervrose@gmail.com>
2020-04-21 13:07:14 +0530

    Add maven gitignores

---
+++ sorts/src/test/java/com/chris/daa4sorts/InsertionSortTest.java
@@ -0,0 +1,18 @@
+package com.chris.daa4sorts;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class InsertionSortTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
